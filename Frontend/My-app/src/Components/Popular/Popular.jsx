ba017ab54a208346510fe0a4557a647326a234ef
saranampranathi <2320030267cseh@gmail.com>
2026-06-22 19:46:26 +0530

    Add files via upload

---
+++ Frontend/My-app/src/Components/Popular/Popular.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Popular.css'
+//import data_product from '../Assets/data'
+
+import Item from '../Item/Item' 
+import { useEffect ,useState } from 'react';
+
+const Popular = () => {
+
+  const[popularproducts,setpopularproducts]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/popularinwomen')
+    .then((response)=>response.json())
+    .then((data)=>setpopularproducts(data));
+  },[])
+
+
+  return (
+    <div className='popular'>
+       <h1>POPULAR SELLS</h1>
+       <hr />
+       <div className="popular-item">
+       {popularproducts.map((item, i) => (
+  <Item
+    key={i}
+    id={item.id}
+    name={item.name}
+    image={item.image}
+    new_price={item.new_price}
+    old_price={item.old_price}
+  />
+))}
+
+       </div>
+        </div>
+  )
+}
+
+export default Popular
